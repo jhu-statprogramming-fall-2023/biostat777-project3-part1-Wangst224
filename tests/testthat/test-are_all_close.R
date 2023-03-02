bc42db096d809dda71bf8266b3e2fa4f75cf83fc
Shengtao Wang <wangst224@gmail.com>
2023-03-01 23:22:30 -0500

    Fix bugs in test: all_are_close

--- tests/testthat/test-are_all_close.R
+++ tests/testthat/test-are_all_close.R
@@ -1,12 +1,10 @@
-test_that("all are close", {
-    # Case 1: correctly return TRUE
+abs_tol = 1e-6
+rel_tol = 1e-6
 
-    abs_tol = 1e-6
-    rel_tol = 1e-6
+test_that("Correctly return TRUE", {
 
-    v = rexp(10)
+    v = c(1, 1)
     w = v
-    w[1] = min(c(w[1] + abs_tol/10, w[1] * (1 + rel_tol/10)))
 
     expect_true(are_all_close(
         v, w, abs_tol = abs_tol, rel_tol = rel_tol
@@ -14,33 +12,23 @@ test_that("all are close", {
 
 })
 
-test_that("all are close: relative error fail", {
-    # Case 2: Relative error above tolerance
+test_that("Relative error fail", {
 
-    abs_tol = 1e-6
-    rel_tol = 1e-6
+    v = c(1, 1)
+    w = c(1, 1 * (1+ rel_tol * 10))
 
-    v = rexp(10)
-    w = v
-    w[1] = w[1] * (1 + rel_tol * 10)
-
-    expect_true(are_all_close(
+    expect_false(are_all_close(
         v, w, abs_tol = abs_tol, rel_tol = rel_tol
     ))
 
 })
 
-test_that("all are close: absolute error fail", {
-    # Case 2: Absolute error above tolerance
+test_that("Absolute error fail", {
 
-    abs_tol = 1e-6
-    rel_tol = 1e-6
+    v = c(1, 1)
+    w = c(1, 1 + abs_tol * 10)
 
-    v = rexp(10)
-    w = v
-    w[1] = w[1] + abs_tol * 10
-
-    expect_true(are_all_close(
+    expect_false(are_all_close(
         v, w, abs_tol = abs_tol, rel_tol = rel_tol
     ))
 
