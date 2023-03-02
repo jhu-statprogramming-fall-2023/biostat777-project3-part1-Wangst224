15c9177533d27b24d1c813d02807aedd1feee9df
Shengtao Wang <wangst224@gmail.com>
2023-03-01 22:54:51 -0500

    Modify test: log likelihood gradient

--- tests/testthat/test-linear_loglikelihood_gradient.R
+++ tests/testthat/test-linear_loglikelihood_gradient.R
@@ -1,12 +1,15 @@
 test_that("linear regression log likelihood gradient", {
 
+    set.seed(224)
     n_obs = 32
     n_pred = 4
 
     data_list = simulate_data(n_obs, n_pred)
 
     LL_gradient = log_likelihood_linear_gradient(data_list$design, data_list$outcome, data_list$coef_true)
-    LL_gradient_numerical = log_likelihood_linear_gradient(data_list$design, data_list$outcome, data_list$coef_true)
+    LL_gradient_numerical = numerical_gradient(function(x) {
+        log_likelihood_linear(data_list$design, data_list$outcome, x)
+    }, data_list$coef_true)
 
 
     abs_tol = 1e-6
