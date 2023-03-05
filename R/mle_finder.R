83b21a651b8652fdbceec842fa3800b06606a81b
Shengtao Wang <wangst224@gmail.com>
2023-03-04 21:02:39 -0500

    Implement logit MLE finder via BFGS

--- R/mle_finder.R
+++ R/mle_finder.R
@@ -22,3 +22,19 @@ find_mle_linear_pseudo_inv = function(design, outcome) {
 
     as.vector(backsolve(upper, backsolve(upper, b, transpose = TRUE)))
 }
+
+find_mle_logit_bfgs = function(design, outcome) {
+
+    n_pred = ncol(design)
+    coeff_init = rep(1, n_pred)
+
+    op_result = optim(coeff_init,
+                      log_likelihood_logit,
+                      log_likelihood_logit_gradient,
+                      design = design,
+                      outcome = outcome,
+                      method = "BFGS",
+                      control = list(fnscale = -1))
+
+    return(op_result$par)
+}
