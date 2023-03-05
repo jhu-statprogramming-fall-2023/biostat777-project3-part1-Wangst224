30cc09166dc4e83136375a9d9f9e32a353b0f6f7
Shengtao Wang <wangst224@gmail.com>
2023-03-04 20:57:15 -0500

    Update hiper_glm: implement logistic regression

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -3,14 +3,14 @@ hiper_glm = function(design, outcome, model = "linear", option = list()){
 
     supported_model = c("linear", "logit")
 
-    if (!(model %in% supported_model)){
+    if (!(model %in% supported_model)) {
         stop(sprintf("The model %s is not supported.", model))
     }
 
     hglm_out = list()
     class(hglm_out) = "hglm"
 
-    if (model == "linear"){
+    if (model == "linear") {
 
         if (is.null(option$mle_finder)) {
             stop("Please specify MLE finder: 'pseudo_inv' or 'bfgs'.")
@@ -26,6 +26,25 @@ hiper_glm = function(design, outcome, model = "linear", option = list()){
 
         else {
             stop("MLE finder not supported. Currently available: 'pseudo_inv' or 'bfgs'.")
+        }
+    }
+
+    else if (model == "logit") {
+
+        if (is.null(option$mle_finder)) {
+            stop("Please specify MLE finder: 'newton' or 'bfgs'.")
+        }
+
+        else if (option$mle_finder == "newton") {
+            hglm_out$coefficients = find_mle_logit_newton(design, outcome)
+        }
+
+        else if (option$mle_finder == "bfgs") {
+            hglm_out$coefficients = find_mle_logit_bfgs(design, outcome)
+        }
+
+        else {
+            stop("MLE finder not supported. Currently available: 'newton' or 'bfgs'.")
         }
     }
 
