fc0724b41e2db93c82ac6d8d8440c8a123105b42
Shengtao Wang <wangst224@gmail.com>
2023-03-05 23:06:00 -0500

    Implement logit MLE finder via Newton method

--- R/mle_finder.R
+++ R/mle_finder.R
@@ -43,13 +43,30 @@ find_mle_logit_newton = function(design, outcome, max_iter = 10000, init = rep(0
 
     coeff = init
     iter = 0
+    abs_tol = 1e-6
+    rel_tol = 1e-6
 
     while (iter < max_iter) {
+        pre_loglikelihood = log_likelihood_logit(design, outcome, coeff)
+
         hessian = log_likelihood_logit_hessian(design, outcome, coeff)
         gradient = log_likelihood_logit_gradient(design, outcome, coeff)
         coeff = coeff - solve(hessian) %*% gradient
 
+        post_loglikelihood = log_likelihood_logit(design, outcome, coeff)
+        abs_change = abs(post_loglikelihood - pre_loglikelihood)
+        rel_change = abs_change / abs(pre_loglikelihood)
+
+        if ((abs_change < abs_tol) | (rel_change < rel_tol)) {
+            break
+        }
+
         iter = iter + 1
+
+    }
+
+    if (iter == max_iter) {
+        warning("Max iteration reached without convergence.")
     }
 
     return(as.vector(coeff))
